e54c03acbe1a5bcebd22d25b88e2e0c7d9945ec4
Viviana Martinez <viviana.mmontesdeoca@gmail.com>
2020-06-09 17:43:42 -0500

    routes fixed

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
   get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit'
 
   patch 'tasks/:id', to: 'tasks#update'
-  delete 'tasks/:id', to: 'tasks#destroy', as: 'destroy'
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
